ec8b464f76ea9f12f463449820fc5067398b7c14
Justin T. Conroy <justin@conroy.in>
2026-05-13 21:13:42 -0500

    Update to newer RMT API

--- ws2812_control.c
+++ ws2812_control.c
@@ -1,77 +1,141 @@
 #include "ws2812_control.h"
-#include "driver/rmt.h"
-#include "esp_err.h"
+
+#include "driver/gpio.h"
+#include "driver/rmt_tx.h"
 #include "esp_check.h"
+#include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "string.h"
 
 // Configure these based on your project needs using menuconfig ********
-#define LED_RMT_TX_CHANNEL		CONFIG_WS2812_LED_RMT_TX_CHANNEL
-#define LED_RMT_TX_GPIO			CONFIG_WS2812_LED_RMT_TX_GPIO
+#define LED_RMT_TX_CHANNEL CONFIG_WS2812_LED_RMT_TX_CHANNEL
+#define LED_RMT_TX_GPIO    CONFIG_WS2812_LED_RMT_TX_GPIO
 // ****************************************************
 
-
 #if CONFIG_WS2812_LED_TYPE_RGB
-#define BITS_PER_LED_CMD	24
+#define BITS_PER_LED_CMD  24
+#define BYTES_PER_LED_CMD 3
 #elif CONFIG_WS2812_LED_TYPE_RGBW
-#define BITS_PER_LED_CMD	32
+#define BITS_PER_LED_CMD  32
+#define BYTES_PER_LED_CMD 4
 #endif
 
-#define LED_BUFFER_ITEMS	(NUM_LEDS * BITS_PER_LED_CMD)
+#define LED_BUFFER_ITEMS (NUM_LEDS * BYTES_PER_LED_CMD)
 
-// These values are determined by measuring pulse timing with logic analyzer and adjusting to match datasheet. 
-#define T0H	CONFIG_WS2812_T0H  // 0 bit high time
-#define T1H	CONFIG_WS2812_T1H  // 1 bit high time
-#define T0L	CONFIG_WS2812_T0L  // low time for either bit
-#define T1L	CONFIG_WS2812_T1L
+// These values are determined by measuring pulse timing with logic analyzer and
+// adjusting to match datasheet.
+#define T0H CONFIG_WS2812_T0H // 0 bit high time
+#define T1H CONFIG_WS2812_T1H // 1 bit high time
+#define T0L CONFIG_WS2812_T0L // low time for either bit
+#define T1L CONFIG_WS2812_T1L
 
 // Tag for log messages
 static const char *TAG = "NeoPixel WS2812 Driver";
 
-// This is the buffer which the hw peripheral will access while pulsing the output pin
-static rmt_item32_t led_data_buffer[LED_BUFFER_ITEMS];
+// This is the buffer which the hw peripheral will access while pulsing the
+// output pin
+static uint8_t led_data_buffer[LED_BUFFER_ITEMS];
+static rmt_channel_handle_t _txChan;
+static rmt_encoder_handle_t _txBytesEncoder;
+static const rmt_transmit_config_t _rmtTxConfig = {
+  .loop_count = 0,
+  .flags =
+    {
+      .eot_level = 0,
+    },
+};
+
+static rmt_symbol_word_t _bit0_symbol = {
+  .duration0 = T0H,
+  .level0    = 1,
+  .duration1 = T0L,
+  .level1    = 0,
+};
+
+static rmt_symbol_word_t _bit1_symbol = {
+  .duration0 = T1H,
+  .level0    = 1,
+  .duration1 = T1L,
+  .level1    = 0,
+};
 
 static void setup_rmt_data_buffer(struct led_state new_state);
 
-esp_err_t ws2812_control_init(void)
-{
-  rmt_config_t config = {
-    .rmt_mode = RMT_MODE_TX,
-    .channel = LED_RMT_TX_CHANNEL,
-    .gpio_num = LED_RMT_TX_GPIO,
-    .mem_block_num = 3,
-    .tx_config.loop_en = false,
-    .tx_config.carrier_en = false,
-    .tx_config.idle_output_en = true,
-    .tx_config.idle_level = 0,
-    .clk_div = 2
+esp_err_t ws2812_control_init(void) {
+  rmt_bytes_encoder_config_t bytes_encoder_config = {
+    .bit0 = _bit0_symbol,
+    .bit1 = _bit1_symbol,
+    .flags =
+      {
+        .msb_first = true,
+      },
+  };
+  ESP_RETURN_ON_ERROR(
+    rmt_new_bytes_encoder(&bytes_encoder_config, &_txBytesEncoder),
+    TAG,
+    "Failed to install bytes encoder");
+
+  rmt_tx_channel_config_t config = {
+    .gpio_num          = LED_RMT_TX_GPIO,
+    .clk_src           = RMT_CLK_SRC_DEFAULT,
+    .resolution_hz     = 40000000, // 40 MHz, 1 tick = 25ns
+    .mem_block_symbols = 128,
+    .trans_queue_depth = 4,
+    .flags =
+      {
+        .invert_out   = false,
+        .with_dma     = false,
+        .io_loop_back = false,
+        // Disable open-drain mode.
+        .io_od_mode = false,
+      },
   };
 
-  ESP_RETURN_ON_ERROR(rmt_config(&config), TAG, "Failed to configure RMT");
-  ESP_RETURN_ON_ERROR(rmt_driver_install(config.channel, 0, 0), TAG, "Failed to install RMT driver");
+  ESP_RETURN_ON_ERROR(rmt_new_tx_channel(&config, &_txChan),
+                      TAG,
+                      "Failed to install RMT TX channel");
+
+  // Disable the pull-up resistor to make the edge transitions fast enough.
+  gpio_set_pull_mode(LED_RMT_TX_GPIO, GPIO_FLOATING);
+
+  ESP_RETURN_ON_ERROR(
+    rmt_enable(_txChan), TAG, "Failed to enable RMT TX Channel");
 
   return ESP_OK;
 }
 
-esp_err_t ws2812_write_leds(struct led_state new_state)
-{
+esp_err_t ws2812_write_leds(struct led_state new_state) {
   setup_rmt_data_buffer(new_state);
-  ESP_RETURN_ON_ERROR(rmt_write_items(LED_RMT_TX_CHANNEL, led_data_buffer, LED_BUFFER_ITEMS, false), TAG, "Failed to write items");
-  ESP_RETURN_ON_ERROR(rmt_wait_tx_done(LED_RMT_TX_CHANNEL, portMAX_DELAY), TAG, "Failed to wait for RMT transmission to finish");
+
+  ESP_RETURN_ON_ERROR(rmt_transmit(_txChan,
+                                   _txBytesEncoder,
+                                   led_data_buffer,
+                                   LED_BUFFER_ITEMS,
+                                   &_rmtTxConfig),
+                      TAG,
+                      "Unable to transmit LED State");
+
+  ESP_RETURN_ON_ERROR(rmt_tx_wait_all_done(_txChan, 50),
+                      TAG,
+                      "Failed while wait form LED state to transmit");
 
   return ESP_OK;
 }
 
-static void setup_rmt_data_buffer(struct led_state new_state)
-{
+static void setup_rmt_data_buffer(struct led_state new_state) {
   for (uint32_t led = 0; led < NUM_LEDS; led++) {
-    uint32_t bits_to_send = new_state.leds[led];
-    uint32_t mask = 1 << (BITS_PER_LED_CMD - 1);
-
-    for (uint32_t bit = 0; bit < BITS_PER_LED_CMD; bit++) {
-      uint32_t bit_is_set = bits_to_send & mask;
-      led_data_buffer[(led * BITS_PER_LED_CMD) + bit] = bit_is_set ?
-                                                      (rmt_item32_t){{{T1H, 1, T1L, 0}}} :
-                                                      (rmt_item32_t){{{T0H, 1, T0L, 0}}};
-      mask >>= 1;
+    uint32_t bytes_to_send = new_state.leds[led];
+    if (BYTES_PER_LED_CMD > 3) {
+      led_data_buffer[(led * BYTES_PER_LED_CMD)]     = bytes_to_send >> 24;
+      led_data_buffer[(led * BYTES_PER_LED_CMD) + 1] = bytes_to_send >> 16;
+      led_data_buffer[(led * BYTES_PER_LED_CMD) + 2] = bytes_to_send >> 8;
+      led_data_buffer[(led * BYTES_PER_LED_CMD) + 3] = bytes_to_send;
+    }
+    else {
+      led_data_buffer[(led * BYTES_PER_LED_CMD)]     = bytes_to_send >> 16;
+      led_data_buffer[(led * BYTES_PER_LED_CMD) + 1] = bytes_to_send >> 8;
+      led_data_buffer[(led * BYTES_PER_LED_CMD) + 2] = bytes_to_send;
     }
   }
 }
